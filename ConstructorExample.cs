15df6c6b217615b20f3747cf3b86e8ed809c733d
Priya2563 <160309884+Priya2563@users.noreply.github.com>
2025-06-28 03:23:47 +0530

    Added constructor example in C#
    This file contains Employee class with both default and parameterized constructors. Used ShowDetails() method to print output.

---
+++ ConstructorExample.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+            {
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee("priya","hrg2",5);
+            emp1.Showdetails();
+            emp2.Showdetails();
+            }
+        
+    }
+        public class Employee
+        {
+            public string Name;
+        public string Department;
+        public int salary;
+        public Employee() {
+            Name = " not assign";
+            Department = "hr";
+            salary = 0;
+        }
+        public Employee(string name, string dept,int sal)
+
+        {
+            Name = name;
+            Department = dept;
+            salary = sal;
+
+        }
+
+
+        
+        public void Showdetails()
+        {
+            Console.WriteLine($"Name:{Name},Department:{Department},salary:{salary}");
+        }
+        }
+
+     
+
+
+    }
+
