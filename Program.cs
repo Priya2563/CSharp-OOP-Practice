51b84623ea1ffded48d2796483d5c63e97148105
Priya2563 <160309884+Priya2563@users.noreply.github.com>
2026-01-30 20:58:40 +0530

    ClassAndObject

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace classAndObject
+{
+
+    class Student
+    {
+        public string name;
+        public int age;
+        public char grade;
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name : {name} , age : {age} , grade : {grade}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.name = "priya";
+            s1.age = 23;
+            s1.grade = 'A';
+            s1.DisplayInfo();
+          
+        }
+    }
+}
