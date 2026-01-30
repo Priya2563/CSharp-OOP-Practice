27a02f94a469171e73a53c9be06373b82f367421
Priya2563 <160309884+Priya2563@users.noreply.github.com>
2026-01-30 21:04:53 +0530

    Constructor Chaining

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConstructorChaining
+{
+    //constructor chaining  inheritance with base keyword
+
+    class Person
+    {
+        public string Name;
+        public Person(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Parent Constructor called for Name: {name}");
+        }
+    }
+    class Employeeee : Person
+    {
+        public int Salary;
+        public Employeeee(string name, int salary) : base(name)
+        {
+            Console.WriteLine($" ChildConstructor called for salary : {salary}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employeeee empp = new Employeeee("Arpit", 10000);
+
+        }
+    }
+}
