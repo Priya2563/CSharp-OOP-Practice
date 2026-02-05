1bc8d5053911cd1e103b1042f2442aea9baf2bb0
Priya2563 <160309884+Priya2563@users.noreply.github.com>
2026-02-05 15:34:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CustomEventArgs_Practice
+{
+   
+    public class TemperatureEventArgs : EventArgs {
+    public int CurrentTemprature { get; set; }
+        public TemperatureEventArgs(int temp)
+        {
+            CurrentTemprature = temp;
+        }
+    }
+    class TemperatureSensor
+    {
+        
+
+        public event EventHandler<TemperatureEventArgs> OnTempratureChanged;
+        private int temperature;
+        public void SetTemperature(int temp)
+        {
+            temperature = temp;
+            Console.WriteLine($"Temperature changed to {temperature}");
+            OnTempratureChanged?.Invoke(this, new TemperatureEventArgs(temperature));
+        }
+
+    }
+
+    internal class Program
+    {
+    
+        public static void HandleTemperature(object sender, TemperatureEventArgs e)
+        {
+            if(e.CurrentTemprature > 40)
+            {
+                Console.WriteLine($"Warning High Temperature:{e.CurrentTemprature}");
+
+
+            }
+            else
+            {
+                Console.WriteLine($"Temperature is Normal : {e.CurrentTemprature}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TemperatureSensor sensor = new TemperatureSensor();
+            sensor.OnTempratureChanged += HandleTemperature;
+            sensor.SetTemperature(25);
+            sensor.SetTemperature(45);
+
+        }
+    }
+}
