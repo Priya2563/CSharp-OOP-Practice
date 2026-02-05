881214401e2f8ffc9b95462f2d3c7444f766f5ce
Priya2563 <160309884+Priya2563@users.noreply.github.com>
2026-02-05 14:30:07 +0530

    I practiced Polymorphism Overloading Here

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PolymorphismMethodOverloading
+{//Polymorpism ka Method Overloading
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Add(2, 3));        // 5
+            Console.WriteLine(calc.Add(2.5, 3.5));    // 6.0
+            Console.WriteLine(calc.Add(1, 2, 3));     // 6
+
+        }
+    }
+}
